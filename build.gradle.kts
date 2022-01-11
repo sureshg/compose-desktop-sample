9dd986550983bd31fce3945dbf3e0dcd7d69a0ce
Suresh G <sureshg.g@gmail.com>
2022-01-11 11:40:37 -0800

    [skip ci]: dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.*
 plugins {
   id("com.google.devtools.ksp") version "1.6.10-1.0.2"
   kotlin("jvm") version "1.6.10"
-  id("org.jetbrains.compose") version "1.1.0-alpha1-dev550"
+  id("org.jetbrains.compose") version "1.1.0-alpha1-dev559"
   id("com.github.ben-manes.versions") version "0.41.0"
   id("com.diffplug.spotless") version "6.1.2"
 }
